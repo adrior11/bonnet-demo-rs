990928f836752eef43d0d545aa809696577beb2a
adrior11 <adrian.schneider11@gmail.com>
2025-01-16 14:21:11 +0000

    Add position struct and stick controlls for circle movements

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+mod position;
+
 use embedded_graphics::{
+    geometry::Point,
     pixelcolor::BinaryColor,
     prelude::*,
     primitives::{Circle, PrimitiveStyle, Rectangle},
@@ -8,6 +11,8 @@ use rppal::gpio::Gpio;
 use ssd1306::{
     mode::BufferedGraphicsMode, prelude::*, size::DisplaySize, I2CDisplayInterface, Ssd1306,
 };
+
+use position::*;
 
 fn main() {
     // Set up I2C device on Raspberry Pi 5
@@ -24,39 +29,63 @@ fn main() {
     display.init().unwrap();
 
     let gpio = Gpio::new().unwrap();
+    // Side buttons
     let button_pin_a = gpio.get(5).unwrap().into_input_pullup();
     let button_pin_b = gpio.get(6).unwrap().into_input_pullup();
+    // Directional stick
+    let button_pin_l = gpio.get(27).unwrap().into_input_pullup();
+    let button_pin_r = gpio.get(23).unwrap().into_input_pullup();
+    let button_pin_u = gpio.get(17).unwrap().into_input_pullup();
+    let button_pin_d = gpio.get(22).unwrap().into_input_pullup();
 
+    let mut pos = Position::new(64, 32);
+
+    // TODO: Improve button processing like in raycast-rs
     loop {
+        if button_pin_l.is_low() {
+            pos.try_move(-1, 0);
+            draw_circle(&mut display, &pos);
+        } else if button_pin_r.is_low() {
+            pos.try_move(1, 0);
+            draw_circle(&mut display, &pos);
+        } else if button_pin_u.is_low() {
+            pos.try_move(0, -1);
+            draw_circle(&mut display, &pos);
+        } else if button_pin_d.is_low() {
+            pos.try_move(0, 1);
+            draw_circle(&mut display, &pos);
+        }
+
         if button_pin_a.is_low() {
-            toggle_circle(&mut display, true);
+            draw_circle(&mut display, &pos);
         } else if button_pin_b.is_low() {
-            toggle_circle(&mut display, false);
+            clear_screen(&mut display);
         }
     }
+
+    // TODO: clear on CTRL-C
 }
 
-fn toggle_circle<DI, SIZE>(
+fn draw_circle<DI, SIZE>(
     display: &mut Ssd1306<DI, SIZE, BufferedGraphicsMode<SIZE>>,
-    on: bool,
+    pos: &Position,
 ) where
     DI: WriteOnlyDataCommand,
     SIZE: DisplaySize,
 {
-    let bin_color = if on {
-        BinaryColor::On
-    } else {
-        BinaryColor::Off
-    };
-    let style = PrimitiveStyle::with_fill(bin_color);
+    let style_fg = PrimitiveStyle::with_fill(BinaryColor::On);
+    let circle = Circle::new(pos.to_point(), DIAMETER).into_styled(style_fg);
+    circle.draw(display).unwrap();
+    display.flush().unwrap();
+}
 
-    if on {
-        let circle = Circle::new(Point::new(64, 32), 20).into_styled(style);
-        circle.draw(display).unwrap();
-    } else {
-        let clear_area = Rectangle::new(Point::new(0, 0), Size::new(127, 63));
-        clear_area.into_styled(style).draw(display).unwrap();
-    }
-
+fn clear_screen<DI, SIZE>(display: &mut Ssd1306<DI, SIZE, BufferedGraphicsMode<SIZE>>)
+where
+    DI: WriteOnlyDataCommand,
+    SIZE: DisplaySize,
+{
+    let style = PrimitiveStyle::with_fill(BinaryColor::Off);
+    let clear_area = Rectangle::new(Point::new(0, 0), Size::new(127, 63));
+    clear_area.into_styled(style).draw(display).unwrap();
     display.flush().unwrap();
 }
---
+++ src/position.rs
@@ -0,0 +1,61 @@
+use std::ops::{Add, Mul};
+
+use embedded_graphics::geometry::Point;
+
+pub const DIAMETER: u32 = 10;
+
+const RADIUS: i32 = DIAMETER as i32 / 2;
+const SPEED: i32 = 1;
+
+const SCREEN_WIDHT: i32 = 128;
+const SCREEN_HEIGHT: i32 = 64;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Position {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+
+    pub fn to_point(&self) -> Point {
+        Point::new(self.x, self.y)
+    }
+
+    pub fn try_move(&mut self, x: i32, y: i32) {
+        let dir = Position::new(x, y);
+        let new_pos = *self + dir * SPEED;
+
+        let out_left = (new_pos.x - RADIUS) < 0;
+        let out_right = (new_pos.x + RADIUS) >= SCREEN_WIDHT;
+        let out_top = (new_pos.y - RADIUS) < 0;
+        let out_bottom = (new_pos.y + RADIUS) >= SCREEN_HEIGHT;
+
+        if !(out_left || out_right || out_top || out_bottom) {
+            *self = new_pos;
+        }
+    }
+}
+
+impl Add<Position> for Position {
+    type Output = Self;
+    fn add(self, rhs: Position) -> Self::Output {
+        Position {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Mul<i32> for Position {
+    type Output = Self;
+    fn mul(self, rhs: i32) -> Self::Output {
+        Position {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
